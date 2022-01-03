7541af3282d6fb812e7da4de79d7d7ea6244db2c
vsagarreddy <vsagar.web@gmail.com>
2022-01-03 23:36:04 +0530

    Implemented firebase utils, included ability to store authenticated users into firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,27 @@ const config = {
     measurementId: "G-3VT0Q0PWHR"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+    const userRef = firestore.doc(`user/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName, email, createdAt, ...additionalData
+            })
+        } catch (error) {
+            console.log('Error while creating user', error.message)
+        }
+    }
+
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
