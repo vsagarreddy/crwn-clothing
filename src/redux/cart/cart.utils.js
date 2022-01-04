55c9c46aa981ed88191cfe320539b07c47825d71
vsagarreddy <vsagar.web@gmail.com>
2022-01-04 14:33:00 +0530

    Updated shop data with new data, Updated cart utils to group items in cart reducer

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,9 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
+
+    if (existingCartItem) {
+        return cartItems.map(item => item.id === cartItemToAdd.id ? { ...item, quantity: item.quantity + 1 } : item)
+    }
+
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
