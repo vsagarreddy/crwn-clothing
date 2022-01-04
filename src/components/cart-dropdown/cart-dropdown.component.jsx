8de384a04b92814f068aac78647d01439e349c56
vsagarreddy <vsagar.web@gmail.com>
2022-01-04 16:18:44 +0530

    Updating our application with reselect, replaced cart selectors with mwmoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 
-import CustomButton from '../custom-button/custom-button.component';
+import CustomButton from "../custom-button/custom-button.component";
 
-import './cart-dropdown.styles.scss';
+import "./cart-dropdown.styles.scss";
+import { connect } from "react-redux";
+import CartItem from "../cart-item/cart-item.component";
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
-const CartDropdown = () => (
-    <div className="cart-dropdown">
-        <div className="cart-items" />
-        <CustomButton>GOT TO CHECKOUT</CustomButton>
+const CartDropdown = ({ cartItems }) => (
+  <div className="cart-dropdown">
+    <div className="cart-items">
+      {cartItems.map((item) => (
+        <CartItem key={item.id} item={item} />
+      ))}
     </div>
+    <CustomButton>GOT TO CHECKOUT</CustomButton>
+  </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
+});
+
+export default connect(mapStateToProps)(CartDropdown);
