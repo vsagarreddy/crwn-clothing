f96ae413634852cfb51902980b7daf938d9bfdec
vsagarreddy <vsagar.web@gmail.com>
2022-01-04 11:41:38 +0530

    Added REDUX, and implemented  to Header and App components

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,7 +14,7 @@ const config = {
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
     if(!userAuth) return;
-    const userRef = firestore.doc(`user/${userAuth.uid}`);
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
     const snapShot = await userRef.get();
 
     if(!snapShot.exists) {
